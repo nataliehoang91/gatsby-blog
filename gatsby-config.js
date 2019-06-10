fbd2465d5046c3b71f2272a660f5b7b98f1ce534
Nat <natalie.hoang91@gmail.com>
2019-06-10 13:23:36 +0700

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Natalie's memories`,
+    author: `Natalie Hoang`,
+    description: `My sharings, my memories.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `kylemathews`,
